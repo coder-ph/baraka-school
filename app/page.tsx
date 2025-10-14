98992f6c19c45f068eb5345b7cda4f2b143d4118
coder-ph <phelixmbani@gmail.com>
2025-10-14 12:42:35 +0300

    home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,45 +8,52 @@ import Footer from "./ui/home/footer";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-gray-50">
-        {/* Sticky Navbar */}
-        <div className="sticky top-0 bg-white z-50">
-          <Navbar />
-          <div className="border-b-2 border-blue-600"></div>
-        </div>
+   
+    <div className="min-h-screen bg-gray-50 ">
+      {/* Sticky Navbar */}
+      <div className="sticky top-0 bg-white z-50">
+        <Navbar />
+        <div className="border-b-2 border-blue-600"></div>
+      </div>
 
-        {/* Hero Carousel Section */}
-        <section className="relative">
-          <Carousel />
-        </section>
+      {/* Hero Carousel Section */}
+      <section className="relative lg:pl-46 lg:pr-46">
+        <Carousel />
+      </section>
 
-        {/* Combined Welcome Card Section */}
-        <section className="py-8 md:py-12">
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="max-w-6xl mx-auto">
+      {/* Combined Welcome Card Section */}
+      <section className="py-5">
+        <div className="container mx-auto">
+          <div className=" mx-auto">
+            {/* Single Combined Card */}
+            <div>
               <CombinedWelcomeCard />
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        {/* Map and Contact Cards Section */}
-        <section className="bg-white py-8 md:py-12">
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-8 max-w-6xl mx-auto">
-              {/* Left Card - Map */}
-              <div className="h-[400px] md:h-[500px] lg:h-[600px]">
-                <MapCard />
-              </div>
+      {/* Map and Contact Cards Section */}
+      <section className="bg-white pb-10">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-8xl mx-auto">
+            {/* Left Card - Map */}
+            <div className="h-[600px]">
+              <MapCard />
+            </div>
 
-              {/* Right Card - Contact Information */}
-              <div className="h-[400px] md:h-[500px] lg:h-[600px]">
-                <ContactCard />
-              </div>
+            {/* Right Card - Contact Information */}
+            <div className="h-[600px] ">
+              <ContactCard />
             </div>
           </div>
-        </section>
-      </div>
-      <Footer />
-    </>
+        </div>
+      </section>
+
+      
+      
+    </div>
+    <Footer />
+     </>
   );
 }
